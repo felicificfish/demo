14ee7d5ac176f9093513ad5361d305e278edf8d6
zhouxy <xiangyu_zhou@jyc99.com>
2019-03-27 19:13:18 +0800

    动态列导出

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,6 +11,10 @@ import com.vdurmont.emoji.EmojiParser;
 import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.jeecgframework.poi.excel.ExcelExportUtil;
+import org.jeecgframework.poi.excel.entity.ExportParams;
+import org.jeecgframework.poi.excel.entity.params.ExcelExportEntity;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,9 +23,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -143,5 +150,75 @@ public class HelloController {
         ips.put("sweet", WebUtil.getRealIpAddr(request));
         ips.put("mine", WebUtil.getIP(request));
         return ips;
+    }
+
+    @GetMapping("dynamic/export")
+    public void dynamicColumnExport(HttpServletResponse response) {
+        try {
+            List<ExcelExportEntity> colList = new ArrayList<ExcelExportEntity>();
+            ExcelExportEntity colEntity = new ExcelExportEntity("商品名称", "title");
+            colEntity.setNeedMerge(true);
+            colList.add(colEntity);
+
+            colEntity = new ExcelExportEntity("供应商", "supplier");
+            colEntity.setNeedMerge(true);
+            colList.add(colEntity);
+
+            ExcelExportEntity deliColGroup = new ExcelExportEntity("得力", "deli");
+            List<ExcelExportEntity> deliColList = new ArrayList<>();
+            deliColList.add(new ExcelExportEntity("市场价", "orgPrice"));
+            deliColList.add(new ExcelExportEntity("专区价", "salePrice"));
+            deliColGroup.setList(deliColList);
+            colList.add(deliColGroup);
+
+            ExcelExportEntity jdColGroup = new ExcelExportEntity("京东", "jd");
+            List<ExcelExportEntity> jdColList = new ArrayList<>();
+            jdColList.add(new ExcelExportEntity("市场价", "orgPrice"));
+            jdColList.add(new ExcelExportEntity("专区价", "salePrice"));
+            jdColGroup.setList(jdColList);
+            colList.add(jdColGroup);
+
+
+            List<Map<String, Object>> list = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                Map<String, Object> valMap = new HashMap<>();
+                valMap.put("title", "名称." + i);
+                valMap.put("supplier", "供应商." + i);
+
+                List<Map<String, Object>> deliDetailList = new ArrayList<>();
+                for (int j = 0; j < 3; j++) {
+                    Map<String, Object> deliValMap = new HashMap<>();
+                    deliValMap.put("orgPrice", "得力.市场价." + j);
+                    deliValMap.put("salePrice", "得力.专区价." + j);
+                    deliDetailList.add(deliValMap);
+                }
+                valMap.put("deli", deliDetailList);
+
+                List<Map<String, Object>> jdDetailList = new ArrayList<>();
+                for (int j = 0; j < 2; j++) {
+                    Map<String, Object> jdValMap = new HashMap<>();
+                    jdValMap.put("orgPrice", "京东.市场价." + j);
+                    jdValMap.put("salePrice", "京东.专区价." + j);
+                    jdDetailList.add(jdValMap);
+                }
+                valMap.put("jd", jdDetailList);
+
+                list.add(valMap);
+            }
+
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("application/x-download");
+            String fileName = URLEncoder.encode("价格分析表.xls", "UTF-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("价格分析表", "数据"),
+                    colList, list);
+            OutputStream fos = response.getOutputStream();
+            workbook.write(fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
